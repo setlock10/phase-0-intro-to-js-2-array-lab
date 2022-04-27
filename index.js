d374fe96cf07d1064baabbb69dc9571a9987b13f
John J Setlock II <johnsetlock@gmail.com>
2022-04-27 11:03:41 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+const cats =["Milo","Otis","Garfield"];
+
+//cats.push("Ralph");
+//cats.unshift("Bob");
+//cats.pop();
+//cats.shift();
+destructivelyAppendCat("Ralph");
+destructivelyPrependCat("Bob");
+destructivelyRemoveLastCat();
+destructivelyRemoveFirstCat();
+
+console.log(cats);
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function  destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function  appendCat(name){
+   const copyCats = [...cats];
+    copyCats.push(name);
+    return copyCats;
+}
+function  prependCat(name){
+    const copyCats = [...cats];
+     copyCats.unshift(name);
+     return copyCats;
+ }
+
+ function removeLastCat(){
+    const copyCats = [...cats];
+    copyCats.pop();
+    return copyCats;  
+}
+function removeFirstCat(){
+    const copyCats = [...cats];
+    copyCats.shift();
+    return copyCats;  
+}
